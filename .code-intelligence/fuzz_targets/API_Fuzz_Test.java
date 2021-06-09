ec17a22756d9e230e649ee71ab0e658b0ea4c9fd
Schrewe <schrewe@code-intelligence.com>
2021-06-09 11:19:05 +0200

    Remove non-working fuzz tests

--- .code-intelligence/fuzz_targets/API_Fuzz_Test.java
+++
@@ -1,8 +0,0 @@
-
-package fuzz_targets;
-
-public class API_Fuzz_Test {
-	public static void fuzzerTestOneInput(byte[] input) {
-		CallYourAPI(input); // TODO call your API here
-	}
-}
--- .code-intelligence/fuzz_targets/API_Fuzz_Test2.java
+++
@@ -1,8 +0,0 @@
-
-package fuzz_targets;
-
-public class API_Fuzz_Test2 {
-	public static void fuzzerTestOneInput(byte[] input) {
-		CallYourAPI(input); // TODO call your API here
-	}
-}
